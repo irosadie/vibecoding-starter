b9af8246831b4eaf1e045657f19b55a49e61bcac
bangimron <imronrosadiee@gmail.com>
2026-03-15 00:28:48 +0700

    feat(web): improve panel command center ux

--- apps/web/app/panel/page.tsx
+++ apps/web/app/panel/page.tsx
@@ -1,5 +1,11 @@
-import PanelContent from "./panel-content"
+import { LoadingSpinner } from "$/components/loading-spinner"
+import { Suspense } from "react"
+import PanelPageContent from "./_components/panel-content"
 
 export default function PanelPage() {
-  return <PanelContent />
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <PanelPageContent />
+    </Suspense>
+  )
 }
